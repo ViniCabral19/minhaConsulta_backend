5959197b351ba806ed9616346b9282ca6a6b4985
vinicabral19 <vini.cabral.basilio@gmail.com>
2024-10-16 09:04:09 -0300

    cp 2 aula 16/10

--- db/database.js
+++ db/database.js
@@ -10,12 +10,15 @@ const db = new sqlite3.Database('./consultas.db', (err) => {
 
 // Criar a tabela de usuários
 db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    role TEXT NOT NULL
-  )`);
+    db.run(`CREATE TABLE IF NOT EXISTS consultations (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      userId INTEGER NOT NULL,
+      date TEXT NOT NULL,
+      doctor TEXT NOT NULL,
+      specialty TEXT NOT NULL,
+      status TEXT NOT NULL,
+      FOREIGN KEY (userId) REFERENCES users(id)
+    )`);
 });
 
 module.exports = db;
